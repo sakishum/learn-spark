df63eb7930caead5c2a0afde280b4e94ab2739ae
migle <longforfreedom+github@gmail.com>
2016-09-08 21:49:58 +0800

    Kafka Consumer

--- code/mkafka/src/main/scala/ConsumerDemo.scala
+++ code/mkafka/src/main/scala/ConsumerDemo.scala
@@ -21,24 +21,29 @@ object ConsumerDemo {
 
 
     val mtot = Map("sdi_scdt_3" -> 1, "sdi_scdt_4" -> 2, "sdi_scdt_5" -> 1)
+
     println(mtot.values.sum)
     val tstreams = consumer.createMessageStreams(mtot)
 
     val executorpool = Executors.newFixedThreadPool(mtot.values.sum)
-
-      tstreams.values.foreach {
-        _.foreach {
-          stream => {
-            executorpool.submit(new MessageHandler(stream))
+      try{
+        tstreams.values.foreach {
+          _.foreach {
+            stream => {
+              executorpool.submit(new MessageHandler(stream))
+            }
           }
         }
+      }finally {
+        executorpool.shutdown()
+        //executorpool.awaitTermination(5000, TimeUnit.MILLISECONDS)
       }
+
+
     downLatch.await()
-    executorpool.shutdown()
-    executorpool.awaitTermination(5000, TimeUnit.MILLISECONDS)
+
     println("关闭consumer")
     //程序异常结束是不是会有offset没有提交?
-    consumer.commitOffsets
     consumer.shutdown()
     println("运行结束")
   }
@@ -49,13 +54,15 @@ object ConsumerDemo {
       try {
         val streamIterator = stream.iterator()
         while (streamIterator.hasNext()) {
+          println("------------------------------------")
+          downLatch.countDown()
           val msgAndMetadata = streamIterator.next()
           println("------------------------------------")
           println(msgAndMetadata.topic)
           println(new String(msgAndMetadata.message()))
           println(msgAndMetadata.offset)
-          println("------------------------------------")
-          downLatch.countDown()
+
+
         }
       } catch {
         case e: Throwable => e.printStackTrace()
