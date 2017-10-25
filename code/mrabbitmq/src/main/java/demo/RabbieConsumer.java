a0a32904b6ecd0b4a70da74e658ed9fc06f2c15d
migle-win <longforfreedom+github@gmail.com>
2017-10-25 16:58:01 +0800

    rabbitmq样例代码

--- code/mrabbitmq/src/main/java/demo/RabbieConsumer.java
+++ code/mrabbitmq/src/main/java/demo/RabbieConsumer.java
@@ -4,10 +4,11 @@ import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -25,21 +26,53 @@ public class RabbieConsumer {
         factory.setPassword("cqcrm");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        channel.basicQos(1); //server push消息时的队列长度
-       switch (RabbitProducerDemo.sendType){
-           case DIRECT:
+        channel.basicQos(
+            1); //prefetch_count=1 setting. This tells RabbitMQ not to give more than one message to a worker at a time. Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one. Instead, it will dispatch it to the next worker that is not still busy.
+        switch (RabbitProducerDemo.sendType) {
+            case DIRECT:
+                //再绑定一个
+                channel.exchangeDeclare(RabbitProducerDemo.exchangeName, "direct");
+                channel.queueDeclare(queueName + "-001", false, false, false, null);
+                String routingKey = "hello";
+                channel.queueBind(queueName + "-001", RabbitProducerDemo.exchangeName, routingKey);
+                break;
+            case FANOUT:
+                channel.exchangeDeclare(RabbitProducerDemo.exchangeName, "fanout");
+                queueName = channel.queueDeclare().getQueue();
 
-               break;
-           case FANOUT:
-               channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-               String queueName = channel.queueDeclare().getQueue();
-               channel.queueBind(queueName, EXCHANGE_NAME, "");
-               break;
+                //在这里可以请创建或者将已有queue绑定到exchange上接收消息
+                channel.queueBind(queueName, RabbitProducerDemo.exchangeName, "");
+                break;
+            case TOPIC:
+                //routingkey是用"."分隔字符串，  "#"匹配一个或多个单词 "*"匹配一个单词
+                //eg:mail.asiainfo.com,可以匹配  #.com和*.asiainfo.com
+                //routingKey = "";
 
-       }
+                channel.exchangeDeclare(RabbitProducerDemo.exchangeName, "topic");
+                queueName = channel.queueDeclare().getQueue();
+                System.out.println("queueName:" + queueName);
+                channel.queueBind(queueName,RabbitProducerDemo.exchangeName,"*.demo.topic");
+                channel.queueBind(queueName,RabbitProducerDemo.exchangeName,"#.demo.topic");
 
-        boolean autoack = false;
-        channel.basicConsume(queueName, autoack, new DefaultConsumer(channel) {
+                //channel.queueBind(queueName,RabbitProducerDemo.exchangeName,"*.demo2.com");
+                break;
+            case HEADERS:
+                channel.exchangeDeclare(RabbitProducerDemo.exchangeName, "headers");
+                queueName = channel.queueDeclare().getQueue();
+                Map<String, Object> headers = new HashMap<String, Object>() {{
+                    put("name", "mi");
+                    put("no", "14341");
+                    put("x-match", "all");//all==匹配所有条件，any==匹配任意条件
+                }};
+                channel.queueBind(queueName, RabbitProducerDemo.exchangeName, "", headers);
+
+                break;
+            default:
+                System.out.println("错误的枚举类型");
+                System.exit(-1);
+        }
+
+        DefaultConsumer myConsumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope,
                 BasicProperties properties,
@@ -50,15 +83,14 @@ public class RabbieConsumer {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                     channel.abort();
-                }finally {
+                } finally {
                     //如果autoack=false,而没有这一句的话服务器不知道此消息已被接收
-                    channel.basicAck(envelope.getDeliveryTag(),false);
+                    channel.basicAck(envelope.getDeliveryTag(), false);
                     //basicQos(1);保证一次只分发一个 。autoAck是否自动回复，如果为true的话，每次生产者只要发送信息就会从内存中删除，那么如果消费者程序异常退出，那么就无法获取数据，我们当然是不希望出现这样的情况，所以才去手动回复，每当消费者收到并处理信息然后在通知生成者。最后从队列中删除这条信息。如果消费者异常退出，如果还有其他消费者，那么就会把队列中的消息发送给其他消费者，如果没有，等消费者启动时候再次发送。
                 }
             }
-        });
-
-
-
+        };
+        boolean autoack = false;
+        channel.basicConsume(queueName, autoack, myConsumer);
     }
 }
--- code/mrabbitmq/src/main/java/demo/RabbitProducerDemo.java
+++ code/mrabbitmq/src/main/java/demo/RabbitProducerDemo.java
@@ -1,10 +1,15 @@
 package demo;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -16,10 +21,14 @@ public class RabbitProducerDemo {
         DIRECT,FANOUT ,TOPIC, HEADERS
     }
 
-    static ExchangeType sendType = ExchangeType.FANOUT;
-    static String exchangeName="rabbit_demo";
+    static ExchangeType sendType = ExchangeType.HEADERS;
+    //static String exchangeName="rabbit_demo";
+    //static String exchangeName="rabbit_demo_direct";
+    //static String exchangeName="rabbit_demo_topic";
+    static String exchangeName="rabbit_demo_headers";
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args)
+        throws IOException, TimeoutException, InterruptedException {
 
         String queueName="hello";
         ConnectionFactory factory = new ConnectionFactory();
@@ -36,27 +45,66 @@ public class RabbitProducerDemo {
         //autoDelete:当连接断开时是否自动删除队列没有连接且没有未处理的消息
         //arguments：建立队列时其他参数
         channel.queueDeclare(queueName,false,false,false,null);
+        String routingKey = queueName;
+        BasicProperties props =  MessageProperties.PERSISTENT_TEXT_PLAIN;
 
         switch(sendType){
             case DIRECT:
                 //默认exchange就是direct不需要特殊处理,(Empty string) and amq.direct:根据Binding指定的Routing Key，将符合Key的消息发送到Binding的Queue
-                //向指定的队列发送消息，消息只会被一个consumer处理,多个消费者消息会轮训处理,消息发送时如果没有consumer，消息不会丢失
+                //direct类型的转发器背后的路由转发算法很简单：消息会被推送至绑定键（binding key）和消息发布附带的选择键（routing key）完全匹配的队列
+                //如果有多个consumer监听了相同的routing key  则他们都会受到消息
+                //最简单的发送方式,routingkey为队列名
+                //任何发往这个exchange的消息都会被路由到routing key的名字对应的队列上，如果没有对应的队列，则消息会被丢弃
+                /////channel.basicPublish("", queueName, null, message.getBytes());
+                //以下代码测试发送至多个queue，发送时会发送到所有绑定的队列中,
+                channel.exchangeDeclare(exchangeName, "direct");
+                routingKey = queueName;
+                //也可以在消费者处绑定
+                channel.queueBind(queueName,exchangeName,routingKey);
+                //再绑定一个,绑定之后才开始发送
+                //channel.queueDeclare(queueName+"-001",false,false,false,null);
+                //channel.queueBind(queueName+"-001",exchangeName,routingKey);
+                //可以绑定到不同queue到routingkey，根据routingkey发送到不同的队列中。
                 break;
             case FANOUT:
-                //广播给所有队列  接收方也必须通过fanout交换机获取消息,所有连接到该交换机的consumer均可获取消息
-                //如果producer在发布消息时没有consumer在监听，消息将被丢弃
-                channel.exchangeDeclare(exchangeName, "fanout");
-                queueName="";
+                //NOTE:
+                //1. 广播给所有队列接收方也必须通过fanout交换机获取消息,所有连接到该交换机的consumer均可获取消息
+                //2. 如果producer在发布消息时没有consumer在监听，消息将被丢弃
+                //3. 给所有与此exchange绑定的queue发送消息，如果没有绑定则不发送
 
+                channel.exchangeDeclare(exchangeName, "fanout");
+                //channel.queueBind("xxxxx",exchangeName,"");
+                routingKey="";
+                break;
             case TOPIC:
+                routingKey="rabbit.demo.topic";
+                channel.exchangeDeclare(exchangeName,"topic");
+                break;
+            case HEADERS:
+                //不是通过routing key来路由消息
+                channel.exchangeDeclare(exchangeName,"headers");
+                Map<String, Object> headers = new HashMap<String, Object>();
+                headers.put("name", "my"); //定义headers
+                headers.put("no", "14341");
+                AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties.Builder().headers(headers);
+
+                props = builder.build();
+                break;
+            default:
+                System.out.println("错误的枚举类型");
+                System.exit(-1);
+
 
         }
+        System.out.println("routing key:"+routingKey);
         for (int i = 0; i < 100; i++) {
             //exchange:若为空则使用默认的exchange,default exchange，它用一个空字符串表示，它是direct exchange类型
 
-            channel.basicPublish(exchangeName,queueName, MessageProperties.PERSISTENT_TEXT_PLAIN,("java-msg-"+i).getBytes("utf-8"));
+            channel.basicPublish(exchangeName,routingKey, props,("java-msg-test-"+i).getBytes("utf-8"));
+            //channel.basicPublish(exchangeName,"rabbit.demo2.topic", MessageProperties.PERSISTENT_TEXT_PLAIN,("java-msg-test-"+i).getBytes("utf-8"));
 
-            System.out.println("java-msg-"+i);
+            System.out.println("java-msg-test-"+i);
+            TimeUnit.MILLISECONDS.sleep(500);
         }
         channel.close();
         connection.close();
