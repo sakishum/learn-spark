dfed8794f78e04c7d2893d7b10987080a1ec6bed
migle-win <longforfreedom+github@gmail.com>
2018-04-27 18:29:08 +0800

    xx

--- code/mspark/src/main/scala/SocialCircle.scala
+++ code/mspark/src/main/scala/SocialCircle.scala
@@ -1,5 +1,3 @@
-import java.io.FileWriter
-/**四川移动家庭圈计算**/
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -8,25 +6,25 @@ import org.apache.spark.{SparkConf, SparkContext}
 //awk -F"," 'NR != 1 {print substr($1,1,11), substr($2,1,11),substr($3,1,1)}' 圈子模型（优化版）预测结果.csv |sort > sc_tmp.txt
 object SocialCircle {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setMaster("local[20]").setAppName("SocialCircle")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("SocialCircle")
     val sc = new SparkContext(conf)
-    //val rdd = sc.textFile("data/social_circle_uniq_sample.txt").filter(!_.isEmpty).filter(!_.startsWith("#")).map(line => {
-    val rdd = sc.textFile("data/social_circle_uniq.data.gz").filter(!_.isEmpty).map(line => {
+    val data = sc.textFile("data/social_circle_uniq_sample.txt").filter(!_.isEmpty).filter(!_.startsWith("#")).map(line => {
+      //val data = sc.textFile("data/social_circle_uniq.data.gz").filter(!_.isEmpty).map(line => {
       val tmp = line.split(" ")
       (tmp(0).trim, tmp(1).trim)
     }).filter(kv => {
       kv._1 != kv._2
-    }).flatMap(kv => Array((kv._1, kv._2), (kv._2, kv._1))).distinct() //A<->B双向关系
+    }).flatMap(kv => Array((kv._1, kv._2), (kv._2, kv._1))) //A<->B双向关系
 
 
     //TODO：按手机号分区？
-    val kvRdd = rdd
-      .groupByKey(200)
+    val kvRdd = data
+      .groupByKey()
       .map(kv => {
         (kv._1, (kv._2.toSet ++ Array(kv._1)))
       })
       .filter(_._2.size > 2) //过滤只有一个直接连接节点的节点(关系是又向的，只删除一个)
-      .sample(false, 0.1)
+      //.sample(false, 0.1)
       .persist(StorageLevel.MEMORY_AND_DISK)
 
     //kvRdd元素本身也构成一个家庭圈
@@ -37,55 +35,61 @@ object SocialCircle {
     //    kvRdd.foreach(println)
     //    println("=" * 10)
 
-    val r = kvRdd
-      .cartesian(kvRdd)
-
+    val r = kvRdd.cartesian(kvRdd)
+    r.foreach(println)
     //reduce?
-    //FIXME:子集没有合并！
-    val circle = r.map({ case (a, b) => {
-      if (a._1 != b._1) {
-        val tmp = a._2.intersect(b._2)
-        //if (f.size >= 3 && f.contains(kv._1._1) && f.contains(kv._2._1)) {  //全连通
-        val tt = (tmp ++ Array(a._1, b._1))
-        if (tt.size >= 3) {
-          tt.toSeq.sortWith(_ < _)
-        } else {
-          Seq()
-        }
-      } else {
-        a._2.toSeq.sortWith(_ < _)
-      }
-    }
-    }).filter(!_.isEmpty).distinct().persist()
-
     //    //FIXME:子集没有合并！
-    //    val circle = r.map(kv => {
-    //      if (kv._1._1 != kv._2._1) {
-    //        val tmp = kv._1._2.intersect(kv._2._2)
+    //    val circle = r.map({ case (a, b) => {
+    //      if (a._1 != b._1) {
+    //        val tmp = a._2.intersect(b._2)
     //        //if (f.size >= 3 && f.contains(kv._1._1) && f.contains(kv._2._1)) {  //全连通
-    //        val tt = (tmp ++ Array(kv._1._1, kv._2._1))
+    //        val tt = (tmp ++ Array(a._1, b._1))
     //        if (tt.size >= 3) {
     //          tt.toSeq.sortWith(_ < _)
     //        } else {
     //          Seq()
     //        }
     //      } else {
-    //        kv._1._2.toSeq.sortWith(_ < _)
+    //        a._2.toSeq.sortWith(_ < _)
     //      }
+    //    }
     //    }).filter(!_.isEmpty).distinct().persist()
 
-    //circle.map(_.mkString("|")).foreach(println)
 
-    val rr = circle.map(_.mkString("|")).collect()
-    val fw = new FileWriter("data/rr.txt")
-    rr.foreach(line => fw.write(line+"\n"))
-    fw.close()
+    val circle = r.map(kv => {
+      if (kv._1._1 != kv._2._1) {
+        if (kv._1._2.subsetOf(kv._2._2)) {
+          kv._2._2.toSeq.sortWith(_ < _)
+        } else {
+          if (kv._2._2.subsetOf(kv._1._2)) {
+            kv._1._2.toSeq.sortWith(_ < _)
+          } else {
+            val tmp = kv._1._2.intersect(kv._2._2)
+            //if (f.size >= 3 && f.contains(kv._1._1) && f.contains(kv._2._1)) {  //全连通
+            val tt = (tmp ++ Array(kv._1._1, kv._2._1))
+            if (tt.size >= 3 && !tt.subsetOf(kv._1._2) && !tt.subsetOf(kv._2._2)) {
+              tt.toSeq.sortWith(_ < _)
+            } else {
+              Seq()
+            }
+          }
+        }
+      } else {
+        kv._1._2.toSeq.sortWith(_ < _)
+      }
+    }).filter(!_.isEmpty) //.distinct().persist()
+
+    circle.map(_.mkString("|")).foreach(println)
+
+    //    val rr = circle.map(_.mkString("|")).collect()
+    //    val fw = new FileWriter("data/rr.txt")
+    //    rr.foreach(line => fw.write(line+"\n"))
+    //    fw.close()
     //repartition(1).saveAsTextFile("data/rr.txt")
 
     //println(r.count())
     //r.foreach(println)
     // println("end.....")
-
     sc.stop()
   }
 }
---
+++ code/mspark/src/main/scala/SocialCircle4S.scala
@@ -0,0 +1,79 @@
+import java.io.FileWriter
+
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * 家庭圈计算
+  * 输入：两两成员关系(成员之两两个关系对是通过业务特征以及其它业务特征分析得出)
+  * 输出：所有成员之间最多经过一个中间成员即可相连则算一个家庭圈，一个成员可以存在于多个家庭圈
+  */
+object SocialCircle4S {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[5]").setAppName("SocialCircle")
+    val sc = new SparkContext(conf)
+    //val data = sc.textFile("data/social_circle_uniq_sample.txt").filter(!_.isEmpty).filter(!_.startsWith("#")).map(line => {
+    val data = sc.textFile("data/social_circle_uniq.data.gz").filter(!_.isEmpty).map(line => {
+      val tmp = line.split(" ")
+      (tmp(0).trim, tmp(1).trim)
+    }).filter(kv => {
+      kv._1 != kv._2
+    }).flatMap(kv => Array((kv._1, kv._2), (kv._2, kv._1))) //A<->B双向关系
+
+
+    //TODO：按手机号分区？
+    val kvRdd = data
+      .groupByKey(50)
+      .map(kv => {
+        (kv._1, (kv._2.toSet + kv._1))
+      })
+      //过滤只有一个直接连接节点的节点(关系是又向的，只删除一个)
+      .filter(_._2.size > 2)
+      .sample(false, 0.001)
+      .persist(StorageLevel.MEMORY_AND_DISK)
+
+    val r = kvRdd.cartesian(kvRdd).persist(StorageLevel.MEMORY_AND_DISK)
+
+    //NOTE:笛卡尔集,所以判断的时候只判断一个方向
+    val circle = r.map({ case (a, b) => {
+      if (a._2.contains(b._1)) { //A,B直接相连
+        if (a._2.subsetOf(b._2)) { //A,B直接相连 且 B和A的所有直连节点都直连
+          (a._2 ++ b._2) //.toSeq.sortWith(_ < _)
+        } else { //A,B直接相连 但 B和A的所有直连节点不全直连
+          //(a._2 + b._1).toSeq.sortWith(_ < _)
+          a._2 //.toSeq.sortWith(_ < _)
+        }
+      } else {
+        if ((a._2 -- b._2) == Set(a._1)) { //A,B不直连，但B和A的所有直接连节点直连
+          (a._2 + b._1) //.toSeq.sortWith(_ < _)
+        } else {
+          a._2 //.toSeq.sortWith(_ < _)
+        }
+      }
+    }
+    }).filter(!_.isEmpty).distinct().persist()
+
+    //FIXME:合并子集
+    val rust = circle.cartesian(circle)
+    val fr = rust.flatMap({
+      case (a, b) => {
+        if (a == b) {
+          Array((a, 0))
+        } else if (a.subsetOf(b)) {
+          Array((a, 1), (b, 0))
+        } else if (b.subsetOf(a)) {
+          Array((a, 0), (b, 1))
+        } else {
+          Array((a, 0), (b, 0))
+        }
+      }
+    })
+
+    val ff = fr.reduceByKey(_ + _).filter(_._2 == 0)
+    val fw = new FileWriter("data/rr.txt")
+    ff.map(_._1.toSeq.sortWith(_ < _)).repartition(1).collect().foreach(line => fw.write(line.mkString("|") + "\n"))
+    fw.close()
+
+    sc.stop()
+  }
+}
